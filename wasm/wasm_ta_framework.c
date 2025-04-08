a254371947babe816867f9b6216ed5ee990f3fcc
liwenxiang1 <liwenxiang1@xiaomi.com>
2025-04-08 14:48:41 +0800

    optee_vela:wasm:fix warnming
    Signed-off-by: liwenxiang1 <liwenxiang1@xiaomi.com>

--- wasm/wasm_ta_framework.c
+++ wasm/wasm_ta_framework.c
@@ -70,14 +70,14 @@ static TEE_Result wasm_copy_in_params(uint32_t param_types, uint32_t* p, TEE_Par
         case TEE_PARAM_TYPE_MEMREF_INPUT:
         case TEE_PARAM_TYPE_MEMREF_OUTPUT:
         case TEE_PARAM_TYPE_MEMREF_INOUT:
-            params[n].memref.size = *((uint32_t*)(p[n]));
-            params[n].memref.buffer = (void*)(p[n] + 4);
+            params[n].memref.size = *((uint32_t*)(uintptr_t)p[n]);
+            params[n].memref.buffer = (void*)(uintptr_t)(p[n] + 4);
             break;
         case TEE_PARAM_TYPE_VALUE_INPUT:
         case TEE_PARAM_TYPE_VALUE_OUTPUT:
         case TEE_PARAM_TYPE_VALUE_INOUT:
-            params[n].value.a = *((uint32_t*)(p[n]));
-            params[n].value.b = *((uint32_t*)(p[n] + 4));
+            params[n].value.a = *((uint32_t*)(uintptr_t)(p[n]));
+            params[n].value.b = *((uint32_t*)(uintptr_t)(p[n] + 4));
             break;
         default:
             EMSG("TEE item not found: 0x%08" PRIx32 "\n", type);
@@ -99,12 +99,12 @@ static void wasm_copy_out_params(uint32_t param_types, uint32_t* p, TEE_Param* p
         switch (type) {
         case TEE_PARAM_TYPE_MEMREF_OUTPUT:
         case TEE_PARAM_TYPE_MEMREF_INOUT:
-            *((uint32_t*)(p[n])) = params[n].memref.size;
+            *((uint32_t*)(uintptr_t)(p[n])) = params[n].memref.size;
             break;
         case TEE_PARAM_TYPE_VALUE_OUTPUT:
         case TEE_PARAM_TYPE_VALUE_INOUT:
-            *((uint32_t*)(p[n])) = params[n].value.a;
-            *((uint32_t*)(p[n] + 4)) = params[n].value.b;
+            *((uint32_t*)(uintptr_t)(p[n])) = params[n].value.a;
+            *((uint32_t*)(uintptr_t)(p[n] + 4)) = params[n].value.b;
             break;
         default:
             break;
@@ -146,7 +146,7 @@ TEE_Result wasm_TA_OpenSessionEntryPoint(uint32_t param_types,
         goto out;
     }
 
-    *sess_ctx = (uint32_t)ctx;
+    *sess_ctx = (uintptr_t)ctx;
 out:
     DMSG("%s ret: 0x%08" PRIx32 " <<\n", __func__, ret);
     return ret;
@@ -160,7 +160,7 @@ void wasm_TA_CloseSessionEntryPoint(uint32_t sess_ctx)
         return;
     }
 
-    user_ta->close_session_entry_point((void*)sess_ctx);
+    user_ta->close_session_entry_point((void*)(uintptr_t)sess_ctx);
     DMSG("%s <<\n", __func__);
 }
 
@@ -187,7 +187,7 @@ TEE_Result wasm_TA_InvokeCommandEntryPoint(uint32_t sess_ctx, uint32_t cmd_id,
         goto out;
     }
 
-    ret = user_ta->invoke_command_entry_point((void*)sess_ctx, cmd_id,
+    ret = user_ta->invoke_command_entry_point((void*)(uintptr_t)sess_ctx, cmd_id,
         param_types, params);
 
     wasm_copy_out_params(param_types, _pl, params);
